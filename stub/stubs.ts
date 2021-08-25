7b290b11b7250955a861ffbcf610e47f46c44275
Ivan Z <corvuss90@gmail.com>
2021-08-25 23:33:40 +0300

    feat: base routing, stub data

---
+++ stub/stubs.ts
@@ -0,0 +1,60 @@
+import { Post, PostContent, PostsList } from '../interfaces/post';
+
+const fakePosts: PostsList = [
+  {
+    id: '1',
+    title: 'First post',
+    annotation: 'some long text',
+  },
+  {
+    id: '2',
+    title: 'First post',
+    annotation: 'some long text',
+  },
+  {
+    id: '3',
+    title: 'First post',
+    annotation: 'some long text',
+  },
+  {
+    id: '4',
+    title: 'First post',
+    annotation: 'some long text',
+  },
+];
+
+const fakePostsContent: Map<string, PostContent> = new Map([
+  [
+    '1',
+    {
+      title: 'First post',
+      content: '<p>This is <strong>first</strong> post</p>',
+    },
+  ],
+  [
+    '2',
+    {
+      title: 'Second post',
+      content: '<p>This is <strong>second</strong> post</p>',
+    },
+  ],
+  [
+    '3',
+    {
+      title: 'Third post',
+      content: '<p>This is <strong>third</strong> post</p>',
+    },
+  ],
+  [
+    '4',
+    {
+      title: 'Fourth post',
+      content: '<p>This is <strong>fourth</strong> post</p>',
+    },
+  ],
+]);
+
+export const stubGetPostsList = (): Promise<PostsList> => Promise.resolve(fakePosts);
+
+export const stubGetPost = (id: Post['id']): Promise<PostContent | undefined> =>
+  Promise.resolve(fakePostsContent.get(id));
