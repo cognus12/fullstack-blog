418d60b1028afd9edd11d738e2a3977f3fedb620
Ivan Z <corvuss90@gmail.com>
2021-09-06 23:44:57 +0300

    feat: footer to bottom

--- components/layout/MainLayout/styles.tsx
+++ components/layout/MainLayout/styles.tsx
@@ -3,6 +3,8 @@ import { FlexContainer } from '../shared';
 
 export const PageWrapper = styled.div`
   min-height: 100vh;
+  display: grid;
+  grid-template-rows: auto 1fr auto;
 `;
 
 export const PageInner = styled(FlexContainer)`
