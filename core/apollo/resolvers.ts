25564a485cfd2802fefee27324fef2dbd80c7739
Ivan Z <corvuss90@gmail.com>
2021-09-14 23:17:25 +0300

    feat(getPosts resolver): use postRepo

--- core/apollo/resolvers.ts
+++ core/apollo/resolvers.ts
@@ -1,14 +1,16 @@
+import { FullPostDTO } from '../db/interfaces/post';
+import { postsRepo } from '../db';
+
+export interface GetPostsArgs {
+  lastId: FullPostDTO['id'];
+}
+
 export const resolvers = {
   Query: {
-    getPosts: () => {
-      return {
-        posts: [
-          {
-            id: '1',
-            title: 'First post',
-          },
-        ],
-      };
+    getPosts: async (_: unknown, _args: GetPostsArgs) => {
+      const { lastId } = _args;
+
+      return await postsRepo.getAll(lastId);
     },
   },
 };
--- core/apollo/schema.ts
+++ core/apollo/schema.ts
@@ -16,6 +16,6 @@ export const typeDefs = gql`
   }
 
   type Query {
-    getPosts: PostData!
+    getPosts(lastId: String): PostData!
   }
 `;
