31c92309b9b6c9fb4ddeb328ac6487fb8cece1d7
Ivan Z <corvuss90@gmail.com>
2021-09-20 22:24:58 +0300

    feat(apollo client): store lastId in cache

--- components/indexPage/PostsWrapper/LoadMore/LoadMore.tsx
+++ components/indexPage/PostsWrapper/LoadMore/LoadMore.tsx
@@ -1,6 +1,12 @@
+import { useQuery } from '@apollo/client';
 import React from 'react';
 import { LoadMoreButton } from './style';
+import { LOCAL_LAST_ID } from '../../../../core/apollo/client';
 
 export const LoadMore: React.FC = () => {
-  return <LoadMoreButton>Load more</LoadMoreButton>;
+  const {
+    data: { lastId },
+  } = useQuery(LOCAL_LAST_ID);
+
+  return <LoadMoreButton onClick={() => console.log(lastId)}>Load more</LoadMoreButton>;
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,27 @@ import Head from 'next/head';
 import { PageSection } from '../components/layout/shared';
 import { PostsWrapper } from '../components/indexPage/PostsWrapper';
 import { postsRepo } from '../core/db';
+import { initializeApollo } from '../core/apollo/client/client';
+import { LOCAL_POSTS_DATA } from '../core/apollo/client';
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const { posts, lastId } = await postsRepo.getAll();
+
+  const apolloClient = initializeApollo();
+
+  apolloClient.cache.writeQuery({
+    query: LOCAL_POSTS_DATA,
+    data: {
+      lastId,
+      posts,
+    },
+  });
 
   return {
     props: {
       posts,
       lastId,
+      initialApolloState: apolloClient.cache.extract(),
     },
   };
 };
