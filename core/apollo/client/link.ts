cc8412b47dff88cbbc4bff531726a00b3d57ad51
Ivan Z <corvuss90@gmail.com>
2021-09-30 00:22:11 +0300

    fix(utils): remove export isServer

--- core/apollo/client/link.ts
+++ core/apollo/client/link.ts
@@ -1,8 +1,8 @@
 import { HttpLink } from '@apollo/client';
 import { SchemaLink } from '@apollo/client/link/schema';
 import { GRAPHQL_PATH } from '../../config/constants';
-import { isServer } from '../../../utils';
 import { clientSchema } from '../schema/clientSchema';
+import { isServer } from '../../../helpers';
 
 export const createIsomorphicLink = () => {
   if (isServer()) {
--- utils/index.ts
+++ utils/index.ts
@@ -2,4 +2,3 @@ export * from './types';
 export * from './pick';
 export * from './omit';
 export * from './takeLast';
-export * from '../helpers/isServer';
