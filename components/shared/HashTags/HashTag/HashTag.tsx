8bdc96cd0f821cef6560abddd24730fce7935320
Ivan Z <corvuss90@gmail.com>
2021-10-08 22:41:21 +0300

    refactor(tags): tags contract rework

--- components/shared/HashTags/HashTag/HashTag.tsx
+++ components/shared/HashTags/HashTag/HashTag.tsx
@@ -1,14 +1,16 @@
 import Link from 'next/link';
 import React from 'react';
 import { Tag } from './style';
-import { HashTagDTO } from '../../../../core/db/interfaces/post';
 
-export interface HashTagProps extends HashTagDTO {}
+export interface HashTagProps {
+  tag: string;
+}
 
-const formatHashTag = (str: string): string => (str[0] === '#' ? str : `#${str}`);
+const formatHashTagToView = (str: string): string => (str[0] === '#' ? str : `#${str}`);
+const formatGashTagToHref = (str: string): string => (str[0] === '#' ? str.slice(1) : str);
 
-export const HashTag: React.FC<HashTagProps> = ({ title, href }) => (
-  <Link href={href}>
-    <Tag>{formatHashTag(title)}</Tag>
+export const HashTag: React.FC<HashTagProps> = ({ tag }) => (
+  <Link href={`/search?tag=${formatGashTagToHref(tag)}`} passHref>
+    <Tag>{formatHashTagToView(tag)}</Tag>
   </Link>
 );
--- components/shared/HashTags/HashTags.tsx
+++ components/shared/HashTags/HashTags.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { HashTagDTO } from '../../../core/db/interfaces/post';
 import { HashTag } from './HashTag';
 import { HashTagsWrapper } from './styles';
 
 export interface HashTagsProps {
-  tags: HashTagDTO[];
+  tags: string[];
   className?: string;
 }
 
@@ -12,7 +11,7 @@ export const HashTags: React.FC<HashTagsProps> = ({ tags, className }) => {
   return (
     <HashTagsWrapper className={className}>
       {tags.map((tag) => (
-        <HashTag key={tag.title} title={tag.title} href={tag.href} />
+        <HashTag key={tag} tag={tag} />
       ))}
     </HashTagsWrapper>
   );
