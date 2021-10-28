1adeeed82f1fe1496fe05b24adab01ed49eb6168
Ivan Zotov <corvuss90@gmail.com>
2021-10-28 22:06:43 +0300

    refactor(graphql client): apollo devtools only in development

--- config/constants.ts
+++ config/constants.ts
@@ -1,3 +1,4 @@
 export const POSTS_PAGE_SIZE = 10;
 export const BASE_URL = 'http://localhost:3000';
 export const GRAPHQL_PATH = '/api/graphql';
+export const ENABLE_APOLLO_DEVTOOLS = process.env.MODE !== 'production';
