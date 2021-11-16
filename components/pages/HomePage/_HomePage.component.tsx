6761279620cfb2b75da3d897ce497c6ff4576745
Ivan Zotov <corvuss90@gmail.com>
2021-11-16 22:31:38 +0300

    refactor(pages): extract duplicated code to PageInner layout

--- components/pages/HomePage/_HomePage.component.tsx
+++ components/pages/HomePage/_HomePage.component.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import Head from 'next/head';
-import { PageSection } from '../../../styles/containers';
 import { PostListLoader } from './PostListLoader';
+import { PageInner } from '../../layout/PageInner';
 
 export const HomePage: React.FC = () => (
-  <>
-    <Head>
-      <title>Create Next App</title>
-    </Head>
-    <PageSection>
-      <PostListLoader />
-    </PageSection>
-  </>
+  <PageInner title="Fullstack blog">
+    <PostListLoader />
+  </PageInner>
 );
