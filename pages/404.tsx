fe7e0b3273b7641dbb5b2d1ef2b244bad364240b
Ivan Zotov <corvuss90@gmail.com>
2021-11-16 22:32:01 +0300

    feat(404): add custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Page404 } from '../components/pages/Page404';
+import { NextPage } from 'next';
+
+const Custom404: NextPage = () => <Page404 />;
+
+export default Custom404;
