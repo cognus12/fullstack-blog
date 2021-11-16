5330d03126138a3881c685f8c683051bc950c47e
Ivan Zotov <corvuss90@gmail.com>
2021-11-16 22:32:01 +0300

    feat(404): add custom 404 page

---
+++ components/pages/Page404/_Page404.component.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { PageInner } from '../../layout/PageInner';
+
+export const Page404: React.FC = () => {
+  return (
+    <PageInner title="404 not found">
+      <>Not found</>
+    </PageInner>
+  );
+};
